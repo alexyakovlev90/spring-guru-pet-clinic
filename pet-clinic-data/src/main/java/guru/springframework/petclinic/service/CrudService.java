e29df1790c67bc2a1136cd9fca5369b63685900a
a.yakovlev9 <a.yakovlev9@tinkoff.ru>
2018-08-27 22:12:19 +0300

    Implement Map Based Services. Closes #6

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/service/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/service/CrudService.java
@@ -12,4 +12,6 @@ public interface CrudService<T, ID> {
     T save(T object);
 
     void delete(T object);
+
+    void deleteById(ID id);
 }
---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/service/map/AbstractMapService.java
@@ -0,0 +1,35 @@
+package guru.springframework.petclinic.service.map;
+
+import guru.springframework.petclinic.model.BaseEntity;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+public class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
+
+    Set<T> findAll() {
+        return new HashSet<>(map.values());
+    }
+
+    T findById(ID id) {
+        return map.get(id);
+    }
+
+    T save(T object) {
+        map.put(object.getId(), object);
+        return object;
+    }
+
+    void deleteById(ID id) {
+        map.remove(id);
+    }
+
+    void delete(T object) {
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+}
