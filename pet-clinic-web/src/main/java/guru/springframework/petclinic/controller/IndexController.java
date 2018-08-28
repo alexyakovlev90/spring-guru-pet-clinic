4116f6175d31420bd8c341508aff616245906fcf
a.yakovlev9 <a.yakovlev9@tinkoff.ru>
2018-08-28 20:46:08 +0300

    IndexController added. Closes #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controller/IndexController.java
@@ -0,0 +1,14 @@
+package guru.springframework.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET, path = {"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
