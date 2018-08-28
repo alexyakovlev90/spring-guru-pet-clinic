ab05d2a3ed17f87f8dea302ea586d941c5e7f362
a.yakovlev9 <a.yakovlev9@tinkoff.ru>
2018-08-28 21:58:39 +0300

    List all Owners on Owner index page. closes #16

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
@@ -1,6 +1,7 @@
 package guru.springframework.petclinic.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class BaseEntity implements Serializable {
 
@@ -12,5 +13,19 @@ public class BaseEntity implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
     }
 }
