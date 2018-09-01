c04f92f7d08f9f8b942adc9d196f11f197076566
a.yakovlev9 <a.yakovlev9@tinkoff.ru>
2018-09-01 15:04:07 +0300

    Update Map setting entities with ID. closes #21

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/service/map/AbstractMapService.java
@@ -2,10 +2,7 @@ package guru.springframework.petclinic.service.map;
 
 import guru.springframework.petclinic.model.BaseEntity;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 
 public class AbstractMapService<T extends BaseEntity, ID extends Long> {
@@ -21,6 +18,13 @@ public class AbstractMapService<T extends BaseEntity, ID extends Long> {
     }
 
     T save(T object) {
+        if (object == null) {
+            throw new RuntimeException("Object is null");
+        }
+
+        if (object.getId() == null) {
+            object.setId(getNextId());
+        }
         map.put(object.getId(), object);
         return object;
     }
@@ -31,5 +35,15 @@ public class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        final Set<Long> longs = map.keySet();
+
+        if (!longs.isEmpty()) {
+            return Collections.max(longs) + 1L;
+        } else {
+            return 1L;
+        }
     }
 }
